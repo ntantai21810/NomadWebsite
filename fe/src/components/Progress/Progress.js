7ace7b364af45a1b8396d740fd642a904dd8430f
huy <69897697+galindo-hoang@users.noreply.github.com>
2021-06-30 15:47:19 +0700

    Comment Progress component

--- fe/src/components/Progress/Progress.js
+++ fe/src/components/Progress/Progress.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./Progress.css";
+// import React from "react";
+// import "./Progress.css";
 
-class Progress extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
+// class Progress extends React.Component {
+//   render() {
+//     return <div></div>;
+//   }
+// }
 
-export default Progress;
+// export default Progress;
