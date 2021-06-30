f9514dfe99824009d97b9f2c65aa43d3eaad287f
huy <69897697+galindo-hoang@users.noreply.github.com>
2021-06-30 16:00:08 +0700

    Modify progress in components

--- fe/src/components/Progress/Progress.js
+++ fe/src/components/Progress/Progress.js
@@ -1,10 +1,20 @@
-// import React from "react";
-// import "./Progress.css";
+import React from "react";
+import "./Progress.css";
 
-// class Progress extends React.Component {
-//   render() {
-//     return <div></div>;
-//   }
-// }
+class Progress extends React.Component {
+  render() {
+    return <div class="Browse-Places">
+    <div class="col col-Browse-Places">Browse Places</div>
+    <div class="row row-icon">
+      <div class="col-md-2" ><a href="" class="a-icon"><i class="fas fa-star favor"></i><div class="text-underline-icon">Favorite</div></a></div>
+      <div class="col-md-2" ><a href="" class="a-icon"><i class="fas fa-fire trend"></i><div class="text-underline-icon">Trending</div></a></div>
+      <div class="col-md-2" ><a href="" class="a-icon"><i class="fas fa-map-marker-alt place"></i><div class="text-underline-icon">Places</div></a></div>
+      <div class="col-md-2" ><a href="" class="a-icon"><i class="fas fa-users user"></i><div class="text-underline-icon">Users</div></a></div>
+      <div class="col-md-2" ><a href="" class="a-icon"><i class="fas fa-money-bill-wave price"></i><div class="text-underline-icon">Prices</div></a></div>
+      <div class="col-md-2" ><a href="" class="a-icon"><i class="fas fa-smile rating"></i><div class="text-underline-icon">Rating</div></a></div>
+    </div>
+  </div>;
+  }
+}
 
-// export default Progress;
+export default Progress;
