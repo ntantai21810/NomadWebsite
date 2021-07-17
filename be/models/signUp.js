25cc5de7d6b4386491cb61321335adb94a698dcb
pearngoc <lethanhngoc123a@gmail.com>
2021-07-17 20:50:16 +0700

    connect to database

--- be/models/signUp.js
+++ be/models/signUp.js
@@ -4,11 +4,21 @@ const signUpTemplate = new mongoose.Schema({
     email:{
         type: String,
         required: true,
+        unique: true
     },
     password:{
         type: String, 
         required: true,
         min: 6
+    },
+    repassword:{
+        type: String, 
+        require: true, 
+        min: 6,
+    },
+    createAt:{
+        type: Date,
+        default: Date.now
     }
 })
 
