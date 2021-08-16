0e9619d0374db01c0dfda6a4fedfad7080fd1e6a
pearngoc <lethanhngoc123a@gmail.com>
2021-08-16 11:11:06 +0700

    update comment

--- be/routes/Post.js
+++ be/routes/Post.js
@@ -13,13 +13,10 @@ router.post("/", async(req, res) =>{
 //update a post
 router.put("/:id", async(req, res)=>{
     try{
+        comments = req.body.comment
         const post = await Post.findById(req.params.id);
-        if(post.userID === req.body.userID){
-            await post.updateOne({$set: req.body});
-            res.status(200).json("the post has been updated");
-        }else{
-            res.status(403).json("You can update only your post")
-        }
+        await post.updateOne({$push: {comment: comments}});
+        res.status(200).json("the post has been updated");
     }catch(err){
         res.status(500).json(err);
     }
